82a4d3c8efb6c5ce52a79d03d055266d3e89c16e
petru <petru_binzari@yahoo.com>
2020-08-07 08:57:06 +0300

    fix: fix order hydrator

--- src/Api/Order.php
+++ src/Api/Order.php
@@ -95,7 +95,7 @@ final class Order extends HttpApi
         }
 
         // Use any valid status code here
-        if (201 !== $response->getStatusCode()) {
+        if (200 !== $response->getStatusCode()) {
             $this->handleErrors($response);
         }
 
